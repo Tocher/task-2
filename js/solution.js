2d42422061116b7cd55131b2724ded6a2cffff76
Alex Kuzmenya <alex.kuzmenya@gmail.com>
2016-03-16 22:35:19 +0300

    Базовая реализация алгоритма

--- js/solution.js
+++ js/solution.js
@@ -4,6 +4,53 @@
     var PATH = root.maze.PATH;
     var CURRENT = root.maze.CURRENT;
 
+    //функция isFinish() проверяет является ли точка выходом из лабиринта
+    function isFinish(x,y,maze){
+        if(x==0 || y==0 || x ==(maze[0].length-1) || y==(maze.length-1)){
+            if(maze[y][x]==0){
+                console.log("finish found at x=" + x+1 +", y=" + y+1 );
+                return true;}
+        }
+        return false;
+    }
+    function check(x,y,maze){
+        if (y>=0 && x>= 0 && y<maze.length && x<maze[0].length ){
+            if(maze[y][x]==0)
+                return true;
+        }
+        return false;
+    }
+
+    function retrace(startX,startY,finishX,finishY,maze){
+        var currentX=finishX,
+            currentY=finishY,
+            distance=maze[finishY][finishX],
+            path=[],
+            result;
+        function findLowerDistance(x,y,dist){
+            if (y-1>=0 && x>= 0 && y-1<maze.length && x<maze[0].length && maze[y-1][x]<distance && maze[y-1][x]!=-1)
+                return [x,y-1,distance-1];
+            if (y>=0 && x-1>= 0 && y<maze.length && x-1<maze[0].length && maze[y][x-1]<distance && maze[y][x-1]!=-1)
+                return [x-1,y,distance-1];
+            if (y+1>=0 && x>= 0 && y+1<maze.length && x<maze[0].length && maze[y+1][x]<distance && maze[y+1][x]!=-1)
+                return [x,y+1,distance-1];
+            if (y>=0 && x+1>= 0 && y<maze.length && x+1<maze[0].length && maze[y][x+1]<distance && maze[y][x+1]!=-1)
+                return [x+1,y,distance-1];
+            return 0;
+        }
+        while(currentX!=startX && currentY!=startY){
+            path.push([currentX,currentY]);
+            if(path.length > 1000) break;
+            result=findLowerDistance(currentX, currentY,distance);
+            currentX=result[0];
+            currentY=result[1];
+            distance=result[2];
+            console.log(result+"");
+        }
+        path.push([currentX,currentY]);
+        path.push([startX,startY]);
+        return path;
+    }
     /**
      * Функция находит путь к выходу и возвращает найденный маршрут
      *
@@ -13,11 +60,72 @@
      * @returns {[number, number][]} маршрут к выходу представленный списоком пар координат
      */
     function solution(maze, x, y) {
-        // todo: построить правильный маршрут к выходу
-        return [
-            [1, 0],
-            [1, 1]
-        ];
+        var start=[x,y],
+        world={0:[start]},          // массив world хранит проверяемые за каждый ход точки, например, за третий ход фунцкия проверила точки world[3]
+        queuedPoints=[start],      //currentPoints хранит точки для следующего хода
+        found = 0, finishX,finishY,
+        turnNumber=0;
+        maze[y][x]=-1;
+        function turn(points){
+            world[++turnNumber]=points;     //толкаем в world массив точек за прошлый ход
+            var newQueue=[];
+            for(var i=0; i<points.length; i++){
+                var x=points[i][0];
+                var y=points[i][1];
+                if (check(x-1,y,maze)){
+                    if(isFinish(x-1,y,maze)){
+                        found=1;
+                        finishX=x;
+                        finishY=y;
+                    }
+                    newQueue.push([x-1,y]);
+                    maze[y][x-1]=turnNumber;
+                }
+                if (check(x,y-1,maze)){
+                    if(isFinish(x,y-1,maze)){
+                        found=1;
+                        finishX=x;
+                        finishY=y;
+                    }
+                    newQueue.push([x,y-1]);
+                    maze[y-1][x]=turnNumber;
+                }
+                if (check(x+1,y,maze)){
+                    if(isFinish(x+1,y,maze)){
+                        found=1;
+                        finishX=x;
+                        finishY=y;
+                    }
+                    newQueue.push([x+1,y]);
+                    maze[y][x+1]=turnNumber;
+                }
+                if (check(x,y+1,maze)){
+                    if(isFinish(x,y+1,maze)){
+                        found=1;
+                        finishX=x;
+                        finishY=y;
+                    }
+                    newQueue.push([x,y+1]);
+                    maze[y+1][x]=turnNumber;
+                }
+            }
+            if (newQueue.length==0) {
+                console.log("Exit not found");
+            }
+            queuedPoints=newQueue;
+
+        }
+
+        while (found!=1) {
+            turn(queuedPoints);
+        }
+        if(found){
+            maze[y][x]=0;
+            var path=retrace(start[0],start[1], finishX, finishY, maze);
+            console.log(start[0], start[1])
+        }
+
+        return path;
     }
 
     root.maze.solution = solution;
